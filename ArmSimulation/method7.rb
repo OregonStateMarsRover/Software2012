d9ef9b05a92be2d7812d6dc4161c4cc235532041
Francis Vo <firefreakfrancis@gmail.com>
2012-01-29 12:02:56 -0800

    updated method7 to be modular

--- ArmSimulation/method7.rb
+++ ArmSimulation/method7.rb
@@ -3,12 +3,8 @@ require 'ruby-processing'
 class ArmSim < Processing::App
 	
 	def setup()
-		@stepSize = 5
-		@seg0 = {:length => 100, :angle=>0,  	:angle2=>0,	:pos => {:x=>300,:y=>500},	:angleLimit =>{	:min => toRad(180.0), 	:max=> toRad(180.0) }}
-		@seg1 = {:length => 140, :angle=>PI/4, 	:angle2=>0,	:pos => {:x=>0,:y=>0},		:angleLimit =>{	:min => toRad(13), 	:max=>toRad(156.0) } }
-		@seg2 = {:length => 130, :angle=>-PI/4,    	:angle2=>0,	:pos => {:x=>0,:y=>0},		:angleLimit =>{	:min => toRad(-182), 	:max=>toRad(22.0) }}
-		@seg3 = {:length => 100, :angle=>PI/6,   	:angle2=>0,	:pos => {:x=>0,:y=>0},		:angleLimit =>{	:min => toRad(-85.0), 	:max=> toRad(94.0) }}
-		@seg = [@seg0,@seg1,@seg2,@seg3]
+		require "Position"
+		setup_seg
 		@endTarget = { :x=>629 , :y=>600 }
 		@target = { :x=>729 , :y=>599 }
 		size(1000, 1000);
@@ -18,12 +14,26 @@ class ArmSim < Processing::App
 		fill(0, 102, 153);
 	end
 
+	def setup_seg
+		@seg0 = {:length => 100, :angle=>0,		:angle2=>0,	:pos => {:x=>300,:y=>500},	:angleLimit =>{	:min => toRad(180.0), 	:max=> toRad(180.0) }}
+		@seg1 = {:length => 140, :angle=>PI/4, 		:angle2=>0,	:pos => {:x=>0,:y=>0},		:angleLimit =>{	:min => toRad(13), 	:max=>toRad(156.0) } }
+		@seg2 = {:length => 130, :angle=>-PI/4,    	:angle2=>0,	:pos => {:x=>0,:y=>0},		:angleLimit =>{	:min => toRad(-182), 	:max=>toRad(22.0) }}
+		@seg3 = {:length => 100, :angle=>PI/6,   	:angle2=>0,	:pos => {:x=>0,:y=>0},		:angleLimit =>{	:min => toRad(-85.0), 	:max=> toRad(94.0) }}
+		@seg = [@seg0,@seg1,@seg2,@seg3]
+	end
+
 	def draw()
 		@angle = 0
 		background(226)
 		
-		
-		
+		display_text
+
+		pushMatrix()
+			draw_arm
+		popMatrix()
+	end
+
+	def display_text
 		text("(#{mouseX}, #{mouseY})",10,40)
 		text("(#{@endTarget[:x]}, #{@endTarget[:y]})",300,40)
 		text("(#{@seg3[:pos][:x]}, #{@seg3[:pos][:y]})",600,40)
@@ -31,96 +41,10 @@ class ArmSim < Processing::App
 		text(string(@seg[1]),10,100);
 		text(string(@seg[2]),10,130);
 		text(string(@seg[3]),10,160);
-		
-		
-		positionSegment(@seg[0], @seg[1])
-		positionSegment(@seg[1], @seg[2])
-		positionSegment(@seg[2], @seg[3])
-		#reachSegment(@seg[3], @endtarget)
-		
-		pushMatrix()
-			drawMethod2
-		popMatrix()
-	end
-	
-	def drawMethod1
-		translate(@seg[0][:pos][:x], @seg[0][:pos][:y])
-		draw_seg(0)
-		draw_seg(1)
-		draw_seg(2)
-		draw_seg(3)
-	end	
-	
-	def drawMethod2
-		nextTarget()
-		#reachSegment(@seg[3], @endtarget)
-		
-		reachSegment(@seg[2], @endTarget)
-		reachSegment(@seg[1], @target)
-		reachSegment(@seg[2], @endTarget)
-		reachSegment(@seg[1], @target)
-		reachSegment(@seg[2], @endTarget)
-		reachSegment(@seg[1], @target)
-		reachSegment(@seg[2], @endTarget)
-		reachSegment(@seg[1], @target)
-		reachSegment(@seg[2], @endTarget)
-		reachSegment(@seg[1], @target)
-		reachSegment(@seg[2], @endTarget)
-		reachSegment(@seg[1], @target)
-		reachSegment(@seg[2], @endTarget)
-		reachSegment(@seg[1], @target)
-		reachSegment(@seg[2], @endTarget)
-		reachSegment(@seg[1], @target)
-		reachSegment(@seg[2], @endTarget)
-		reachSegment(@seg[1], @target)
-		translate(@seg[0][:pos][:x], @seg[0][:pos][:y])
-		
-		strokeWeight(10)
-		draw_seg2(0)
-		draw_seg2(1)
-		draw_seg2(2)
-		draw_seg2(3)
-	end
-	
-	def draw_seg(i)
-		seg = @seg[i]
-		draw_limit(seg)
-		strokeWeight(3)
-		rotate(-seg[:angle])
-		@angle += seg[:angle]
-		seg[:angle2] = @angle
-		positionSegment(seg, @seg[i+1]) if i <3 
-		line(0, 0, seg[:length] , 0)
-		translate(seg[:length], 0)
-	end
-	
-	def draw_seg2(i)
-		strokeWeight(10)
-		seg = @seg[i]
-	
-		rotate(seg[:angle2])
-		line(0, 0, seg[:length] , 0)
-		translate(seg[:length], 0)
-		draw_limit(@seg[i+1]) if (i < 3)
-		rotate(-seg[:angle2])
-	end
-	
-	def reachSegment(seg, target) 
-		dx = target[:x] - seg[:pos][:x]
-		dy = target[:y] - seg[:pos][:y]
-		da = atan2(dy, dx) - seg[:angle2]
-		seg[:angle2] +=da
-		@target[:x] = target[:x] - cos(seg[:angle2]) * seg[:length]
-		@target[:y] = target[:y] - sin(seg[:angle2]) * seg[:length]
 	end
 	
 	def mousePressed
 		@endTarget = {:x=>mouseX,:y=>mouseY}
-	end
-	
-	def positionSegment(a, b) 
-		b[:pos][:x] = a[:pos][:x] + cos(a[:angle2]) * a[:length]
-		b[:pos][:y] = a[:pos][:y] + sin(a[:angle2]) * a[:length]
 	end
 	
 	def draw_limit(seg)
@@ -166,6 +90,11 @@ class ArmSim < Processing::App
 			@target[:y] = @endTarget[:y] 
 		end
 	
+	end
+
+	def positionSegment(a, b) 
+		b[:pos][:x] = a[:pos][:x] + cos(a[:angle2]) * a[:length]
+		b[:pos][:y] = a[:pos][:y] + sin(a[:angle2]) * a[:length]
 	end
 
 	def endPoint
