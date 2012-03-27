3cd982f122871a74a463b4eae8062c87210fdd07
Francis Vo <firefreakfrancis@gmail.com>
2012-03-27 09:23:31 -0700

    starting Wheel sim

---
+++ WheelSimulation/WheelSim.rb
@@ -0,0 +1,20 @@
+
+require 'ruby-processing'
+#require 'joystick'
+#require 'mkmf'
+#create_makefile('joystick')
+class ArmSim < Processing::App
+	def setup
+		setup_wheels
+	end
+
+	def setup_wheels
+		
+	end
+
+	def draw
+
+	end
+
+
+end	
